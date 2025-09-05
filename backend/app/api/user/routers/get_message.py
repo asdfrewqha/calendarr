3a16977a4c5583f144aece63e16123a580d746ec
asdfrewqha <asdfrewqha@gmail.com>
2025-09-05 19:30:03 +0300

    Create get_message.py

---
+++ backend/app/api/user/routers/get_message.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.database.adapter import adapter
+from app.database.models import Message, User
+from app.database.session import get_async_session
+from app.api.user.schemas import MessageScheme
+from app.dependencies.checks import check_user_token
+from typing import Annotated
+from uuid import UUID
+
+router = APIRouter()
+
+
+@router.get("/get-msg/{msg_id}", response_model=MessageScheme)
+async def list_message(
+    user: Annotated[User, Depends(check_user_token)],
+    session: Annotated[AsyncSession, Depends(get_async_session)],
+    msg_id: UUID
+):
+    msg = await adapter.get_by_id(Message, msg_id, session)
+    return (MessageScheme.model_validate(msg, from_attributes=True))
