e0fbb08de2a8f401a548672b92d26e0fb218b396
asdfrewqha <asdfrewqha@gmail.com>
2025-09-05 17:52:14 +0300

    Update list_messages.py

--- backend/app/api/user/routers/list_messages.py
+++ backend/app/api/user/routers/list_messages.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from datetime import datetime, time, date
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database.adapter import adapter
@@ -13,18 +13,18 @@ router = APIRouter()
 
 @router.get("/list-msg")
 async def list_message(
-    start_date: date,
-    end_date: date,
     user: Annotated[User, Depends(check_user_token)],
-    session: Annotated[AsyncSession, Depends(get_async_session)]
+    session: Annotated[AsyncSession, Depends(get_async_session)],
+    start_date: Query[None, date],
+    end_date: Query[..., date],
 ):
     start_date = datetime.combine(start_date, time())
     if end_date:
         end_date = datetime.combine(end_date, time(23, 59, 59))
-        list_messages = await adapter.get_by_cond(Message, "end_send_date", start_date, ">=", "end_send_date", "<=", end_date, "user_id", user.id, "==", session=session)
+        list_messages = await adapter.get_by_cond(Message, "end_send_date", start_date, ">=", "end_send_date", end_date, "<=", "user_id", user.id, "==", session=session)
     else:
         end_date = datetime.combine(start_date, time(23, 59, 59))
-        list_messages = await adapter.get_by_cond(Message, "end_send_date", start_date, ">=", "end_send_date", "<=", end_date, "user_id", user.id, "==", session=session)
+        list_messages = await adapter.get_by_cond(Message, "end_send_date", start_date, ">=", "end_send_date", end_date, "<=", "user_id", user.id, "==", session=session)
     response = []
     for msg in list_messages:
         response.append(MessageScheme.model_dump(msg))
