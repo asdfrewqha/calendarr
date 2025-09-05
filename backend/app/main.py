ccc17384d4dbc1ed0a5f0fc395f01e89368f9c7c
asdfrewqha <asdfrewqha@gmail.com>
2025-09-05 14:17:08 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,7 +33,7 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://127.0.0.1:8000"],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
