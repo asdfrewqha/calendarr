c6de737bc4ada85fefdc121a4d2664bdf3b85fad
asdfrewqha <asdfrewqha@gmail.com>
2025-09-05 22:51:54 +0300

    Update refresh.py

--- backend/app/api/auth/routers/refresh.py
+++ backend/app/api/auth/routers/refresh.py
@@ -17,7 +17,7 @@ router = APIRouter()
 @router.get("/refresh", status_code=status.HTTP_200_OK)
 async def refresh(response: Response, user: Annotated[User, Depends(check_refresh)]):
     response.set_cookie("access_token", TokenManager.create_token(
-        {"sub": str(user.id), "type": "access"},
+        {"sub": int(user.id), "type": "access"},
         TokenManager.ACCESS_TOKEN_EXPIRE_MINUTES,
     ))
     return okresponse()
