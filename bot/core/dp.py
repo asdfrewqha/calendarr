a7dc82892132539b734e8228899453f5e0f55cdf
asdfrewqha <asdfrewqha@gmail.com>
2025-09-06 01:30:07 +0300

    a

--- bot/core/dp.py
+++ bot/core/dp.py
@@ -1,5 +1,3 @@
-import re
-
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from core.config import BOT_TOKEN
@@ -10,17 +8,11 @@ dp = Dispatcher(storage=MemoryStorage())
 dp.include_router(router)
 
 
-def escape_markdown(text: str) -> str:
-    escape_chars = r"[_\[\]()~`>#+\-=|{}.!]"
-    return re.sub(f"({escape_chars})", r"\\\1", text)
-
-
 async def send_message(chat_id: int, text: str):
     await bot.send_message(chat_id=chat_id, text=text)
 
 
 async def send_md_message(chat_id: int, text: str):
-    escaped_text = escape_markdown(text)
     await bot.send_message(
-        chat_id=chat_id, text=escaped_text, parse_mode="MarkdownV2", disable_web_page_preview=True
+        chat_id=chat_id, text=text, parse_mode="HTML", disable_web_page_preview=True
     )
