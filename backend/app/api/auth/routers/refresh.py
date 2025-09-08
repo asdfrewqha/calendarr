9166178462bfcd0512aad7d8cf36318ec81af051
asdfrewqha <asdfrewqha@gmail.com>
2025-09-08 07:14:19 +0300

    Update refresh.py

--- backend/app/api/auth/routers/refresh.py
+++ backend/app/api/auth/routers/refresh.py
@@ -7,18 +7,18 @@ from app.utils.token_manager import TokenManager
 from fastapi import APIRouter, Depends, Response, status
 
 logger = get_logger()
-
-
 router = APIRouter()
 
 
 @router.get("/refresh", status_code=status.HTTP_200_OK)
 async def refresh(response: Response, user: Annotated[User, Depends(check_refresh)]):
-    response.set_cookie(
-        "access_token",
-        TokenManager.create_token(
-            {"sub": int(user.id), "type": "access"},
-            TokenManager.ACCESS_TOKEN_EXPIRE_MINUTES,
-        ),
+    access_token = TokenManager.create_token(
+        {"sub": int(user.id), "type": "access"},
+        TokenManager.ACCESS_TOKEN_EXPIRE_MINUTES,
     )
-    return response
+
+    response.set_cookie(
+        key="access_token", value=access_token, httponly=True, secure=True, samesite="None"
+    )
+
+    return {"message": "Token refreshed successfully", "user_id": user.id}
