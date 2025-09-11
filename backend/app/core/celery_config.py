123c2769e35219489cdf16126f173ec5fa490700
asdfrewqha <asdfrewqha@gmail.com>
2025-09-11 23:35:49 +0300

    Update celery_config.py

--- backend/app/core/celery_config.py
+++ backend/app/core/celery_config.py
@@ -3,14 +3,17 @@ from app.core.logging import get_logger
 from app.core.settings import settings
 
 logger = get_logger()
-logger.info("Initializing Celery")
 
-app = Celery()
+logger.info(settings.rbmq.celery_url)
 
-app.conf.update(
-    broker_url=settings.rbmq.celery_url,
-    result_backend=settings.redis_settings.redis_url,
-)
+app = Celery(main="app", broker=settings.rbmq.celery_url, backend=settings.redis_settings.redis_url)
+
+app.conf.task_serializer = "json"
+app.conf.result_serializer = "json"
+app.conf.accept_content = ["json"]
+
+app.conf.task_acks_late = True
+app.conf.worker_prefetch_multiplier = 1
+app.conf.broker_heartbeat = 60
 
 app.autodiscover_tasks(packages=["app.api.user"])
-logger.info("Celery configuration loaded successfully")
