f8d7bb0bb59b5bb8621aa6cd9ce730afc10d46db
asdfrewqha <asdfrewqha@gmail.com>
2025-09-16 01:25:29 +0300

    Update schedule_task.py

--- backend/app/utils/schedule_task.py
+++ backend/app/utils/schedule_task.py
@@ -7,10 +7,10 @@ from taskiq import ScheduledTask
 
 async def schedule(msg_id: UUID, user_id: int, send_date: datetime):
     scheduled_task_1 = ScheduledTask(
-        schedule_id=str(msg_id),
         task_name="send_delayed_message",
-        args=[user_id, msg_id],
-        time=send_date,
         labels={"chat_id": str(user_id), "scheduled_time": send_date.isoformat()},
+        args=[user_id, msg_id],
+        schedule_id=str(msg_id),
+        time=send_date,
     )
     await source.add_schedule(scheduled_task_1)
